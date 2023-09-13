2087d7cf4e4f00fe33c663c84066031a4ab94c07
Ashley Babjac <ababjac@vols.utk.edu>
2023-09-13 08:51:58 -0500

    updating figures

--- scripts/performance_plot.py
+++ scripts/performance_plot.py
@@ -10,6 +10,6 @@ files = [os.path.join(DIR, f) for f in os.listdir(DIR) if os.path.isfile(os.path
 for f in files:
     print(f)
     df = pd.read_csv(f, index_col=0)
-    sns.barplot(data=df, x='Phylum Name', y='AUC', hue='Shuffled', capsize=0.2)
+    sns.barplot(data=df, x='Phylum Name', y='AUC', hue='Shuffled', capsize=0.2, orient="v")
     plt.savefig(f.partition('.csv')[0].replace('files', 'figures')+'.png')
     plt.close()
