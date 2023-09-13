a192f95c39246842334f5d5eb7f9f0790e8c59d5
Ashley Babjac <ababjac@vols.utk.edu>
2023-09-13 09:39:59 -0500

    updating figures

--- scripts/performance_plot.py
+++ scripts/performance_plot.py
@@ -8,8 +8,13 @@ DIR = 'files/performance/'
 files = [os.path.join(DIR, f) for f in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, f))]
 
 for f in files:
-    print(f)
+    #print(f)
+
+    data, _, typ = f.split('-')
+    typ = typ.partition('.')[0]
     df = pd.read_csv(f, index_col=0)
     sns.barplot(data=df, x='AUC', y='Phylum Name', hue='Shuffled', capsize=0.2, orient="h")
+    plt.title('AUC Performance - '+data+' '+typ)
+    plt.tight_layout()
     plt.savefig(f.partition('.csv')[0].replace('files', 'figures')+'.png')
     plt.close()
