5cb0017b562a65eb67843dd5505417602f78b44d
Ashley Babjac <ababjac@vols.utk.edu>
2023-09-13 09:51:27 -0500

    fixing directory

--- scripts/performance_plot.py
+++ scripts/performance_plot.py
@@ -8,12 +8,13 @@ DIR = 'files/performance/'
 files = [os.path.join(DIR, f) for f in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, f))]
 
 for f in files:
-    #print(f)
+    print(f)
 
     data, _, typ = f.split('-')
     data = data.split('/')[-1]
     typ = typ.partition('.')[0]
     df = pd.read_csv(f, index_col=0)
+    df.sort_values(by='Phylum Name')
     sns.barplot(data=df, x='AUC', y='Phylum Name', hue='Shuffled', capsize=0.2, orient="h")
     plt.title('AUC Performance - '+data+' '+typ)
     plt.legend(bbox_to_anchor=(1.02, 0.55), loc='upper left', borderaxespad=0)
